32ff7d148d9e83956c7895c6616cc79dd877a08a
JosephPena <arcx354@gmail.com>
2016-09-27 18:15:47 -0400

    (some outline of what functionality i want JS/jQuery)

--- application.js
+++ application.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
+	function compChoice(){
+		// function which will determine what the computer has to choose againt the user
+	}
+
+	function matchup(){
+		//  eventually matching up the user against the computer option
+	}
 
 
-
-	
 })

1de49406c8aadec156c3e3c33ff9b4a336d8dfdb
JosephPena <arcx354@gmail.com>
2016-09-27 18:17:19 -0400

    (minor update to js/jquery file)

--- application.js
+++ application.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+		//  jquery function which will highlight a selector + event + event handler listeining for user selection
+
 	function compChoice(){
 		// function which will determine what the computer has to choose againt the user
 	}

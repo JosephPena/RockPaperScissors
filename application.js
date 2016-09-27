6a6c97fc4dcf0ffe94aab258ee8f24c42f2f2459
JosephPena <arcx354@gmail.com>
2016-09-27 18:22:47 -0400

    (more notes and bit more functionality)

--- application.js
+++ application.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
 	function compChoice(){
 		// function which will determine what the computer has to choose againt the user
+		//choice options will randomize from 0 to 2 inclusive
+		var choice = Math.floor(Math.random()*3);
 	}
 
 	function matchup(){

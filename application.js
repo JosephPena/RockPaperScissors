cb731432d8093ee4c35c381563259a59b67b76b6
JosephPena <arcx354@gmail.com>
2016-09-28 12:02:41 -0400

    (added jQuery & JS functionality)

--- application.js
+++ application.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
 		//  jquery function which will highlight a selector + event + event handler listeining for user selection
 
-		$("a").on("click", function(){
-			if($(this).attr("id") === "rock"){
-				// do stuff
-			}else if($(this).attr("id") === "paper"){
-				// do stuff
-			}else if ($(this).attr("id") === "scissors"){
-				// do stuff
-			}
-		});
+	$("a").on("click", function(){
+		if($(this).attr("id") === "rock"){
+			console.log("rock");
+		}else if($(this).attr("id") === "paper"){
+			console.log("paper");
+		}else if ($(this).attr("id") === "scissors"){
+			console.log("scissors");
+		}
+	});
 
 //Computer Function that randomly selects a value between 1-3.  Tied to a rock,paper,scissor option
 	function compChoice(){
@@ -25,11 +25,18 @@ $(document).ready(function(){
 			result = "scissors";
 		}
 		return result;
-	}
+	};
 
-	function matchup(){
+	function matchup(input){
 		//  eventually matching up the user against the computer option
-	}
+		if(input === "rock"){
+			console.log("user eval-rock working")
+		}else if (input === "paper"){
+			console.log("user eval-paper working")
+		}else if (input === "scissors") {
+			console.log("user eval-scissors working")
+		}
+	};
 
 
-})
+});
